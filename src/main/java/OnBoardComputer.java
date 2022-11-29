a8bfd26f76fd7f06ebf80ca80608347a23ba4883
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-11-29 15:08:26 -0500

    all tests passing

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,6 +3,41 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
+        if(status.getAltitude() > 15000){
+            burn = 0;
+        }
+        else if (status.getAltitude() >= 1000 && status.getVelocity() >100){
+            burn = 200;
+        }   else if(status.getAltitude() > 1000 && status.getVelocity() <= 100){
+            burn = 0;
+        }
+        else if(status.getAltitude() > 200 && status.getVelocity() > 100){
+            burn = 200;
+        }
+        else if(status.getAltitude() > 200 && status.getVelocity() == 100){
+            burn = 100;
+        }
+        else if(status.getAltitude() > 50 && status.getVelocity() > 100){
+            burn = 200;
+        }
+        else if(status.getAltitude() > 50 && status.getVelocity() > 25){
+            burn = 175;
+        }
+        else if(status.getAltitude() > 50 && status.getVelocity() == 25){
+            burn = 100;
+        }
+        else if(status.getAltitude() > 10 && status.getVelocity() > 10){
+            burn = 115;
+        }
+        else if(status.getAltitude() > 10 && status.getVelocity() == 10){
+            burn = 100;
+        }
+        else if(status.getAltitude() <= 10 && status.getVelocity() >= 10){
+            burn = 100 + (status.Velocity - 1);
+        }
+        else if(status.getAltitude() < 10 && status.getVelocity() == 1){
+            burn = 100;
+        }
         
         System.out.println(burn); /*hack!*/
         return burn;
